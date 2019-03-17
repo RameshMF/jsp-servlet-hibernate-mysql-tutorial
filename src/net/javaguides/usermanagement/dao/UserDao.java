6a8a80fccd4570ccfa5dd5a2e277aa56ecd710dd
Ramesh Fadatare <rfadatare@avaya.com>
2019-03-17 10:32:10 +0530

    jsp-servlet-hibernate-crud-example

---
+++ src/net/javaguides/usermanagement/dao/UserDao.java
@@ -0,0 +1,139 @@
+package net.javaguides.usermanagement.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import net.javaguides.usermanagement.model.User;
+import net.javaguides.usermanagement.utl.HibernateUtil;
+
+/**
+ * CRUD database operations
+ * @author Ramesh Fadatare
+ *
+ */
+public class UserDao {
+	
+	/**
+	 * Save User
+	 * @param user
+	 */
+	public void saveUser(User user) {
+		Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+			// save the student object
+			session.save(user);
+			// commit transaction
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Update User
+	 * @param user
+	 */
+	public void updateUser(User user) {
+		Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+			// save the student object
+			session.update(user);
+			// commit transaction
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Delete User
+	 * @param id
+	 */
+	public void deleteUser(int id) {
+
+		Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+
+			// Delete a user object
+			User user = session.get(User.class, id);
+			if (user != null) {
+				session.delete(user);
+				System.out.println("user is deleted");
+			}
+
+			// commit transaction
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Get User By ID
+	 * @param id
+	 * @return
+	 */
+	public User getUser(int id) {
+
+		Transaction transaction = null;
+		User user = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+			// get an user object
+			user = session.get(User.class, id);
+			// commit transaction
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+		return user;
+	}
+	
+	/**
+	 * Get all Users
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public List<User> getAllUser() {
+
+		Transaction transaction = null;
+		List<User> listOfUser = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+			// get an user object
+			
+			listOfUser = session.createQuery("from User").getResultList();
+			
+			// commit transaction
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+		return listOfUser;
+	}
+}
---
+++ src/net/javaguides/usermanagement/model/User.java
@@ -0,0 +1,77 @@
+package net.javaguides.usermanagement.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * User.java
+ * This is a model class represents a User entity
+ * @author Ramesh Fadatare
+ *
+ */
+
+@Entity
+@Table(name="instructor")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	protected int id;
+	
+	@Column(name="name")
+	protected String name;
+	
+	@Column(name="email")
+	protected String email;
+	
+	@Column(name="country")
+	protected String country;
+	
+	public User() {
+	}
+	
+	public User(String name, String email, String country) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.country = country;
+	}
+
+	public User(int id, String name, String email, String country) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.country = country;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+}
